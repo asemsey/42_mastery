b2c38e6c152146842bcc645000ef28d24883c4f1
Anna Semsey <annasemsey@gmail.com>
2025-07-05 15:46:29 +0200

    libft bonus with ft_getenv

--- ft_ls/include/ft_ls.h
+++ ft_ls/include/ft_ls.h
@@ -10,13 +10,15 @@
 # include <time.h>
 # include <dirent.h>
 
+# include "../libft/libft.h"
+
 // data -------------------------------------
 
 enum e_ftype {
-	DIR,
-	LINK,
-	FILE,
-	SOCK
+	F_DIR,
+	F_LINK,
+	F_FILE,
+	F_SOCK
 };
 
 typedef struct s_filedata {
@@ -26,7 +28,7 @@ typedef struct s_filedata {
 	int permissions;// format of chmod - three bits?
 	time_t created;
 	time_t modified;
-	t_filedata **files;// if f_type is dir this contains files -- should this be linkedlist?
+	struct s_filedata **files;// if f_type is dir this contains files -- should this be linkedlist?
 	int num_files;// len of `files`
 }	t_filedata;
 
--- ft_ls/src/main.c
+++ ft_ls/src/main.c
@@ -1,10 +1,15 @@
 #include "../include/ft_ls.h"
 
 int	main(int argc, char **argv, char **env) {
+	(void)argc;
+	(void)argv;
+	// (void)env;
 	t_filedata pwd;
 
-	// set name to pwd dir
-	// 
+	pwd.f_type = F_DIR;
+	pwd.name = ft_getenv("PWD", env);
+	if (!pwd.name)
+		return 1;
+	ft_putendl_fd(pwd.name, 1);
 	return 0;
 }
-
